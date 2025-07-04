c8cd7c2fd2b5ab6b8854809b7ac7653fe7272203
André Mateus <andrem.03p@gmail.com>
2025-07-04 12:30:50 -0300

    Adição da tela de instrução e título arrumado

--- bleh/main.c
+++ bleh/main.c
@@ -1,8 +1,10 @@
 //Headers
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <string.h>
 
-//Constantes
+//Constantes globais
 #define tamTab 10
 
 //===================================================================================================================================================================================================
@@ -12,20 +14,22 @@
 //Desenha a tela de título no console. (Bastante óbvio.)
 void telaTitulo() {
     void telaInstrucao();
+    char *titulo[] = {
+        " _  _ __ __ _  _  _  __   ___  __     ____ ____ ____  ___  __  _  _ ____ ____ __ _  __  ____  __  ",
+        "( \\/ |  |  ( \\/ )( \\/  \\ / __)/ _\\   (    (  __) ___)/ __)/  \\/ )( (  __|  _ (  ( \\/ _\\(    \\/ _\\ ",
+        "/ \\/ \\)(/    /) __ (  O | (__/    \\   ) D () _)\\___ ( (_ (  O ) \\/ /) _) )   /    /    \\) D (    \\ ",
+        "\\_)(_(__)_)__)\\_)(_/\\__/ \\___)_/\\_/  (____(____|____/\\___/\\__/ \\__/(____|__\\_)_)__)_/\\_(____|_/\\_/"
+    };
+    int tamArr = sizeof(titulo)/sizeof(titulo[0]);
 
     //Título
     for(int i = 0; i < 10; i++) { printf("\n"); }
-    for(int  i = 0; i < 15; i++) { printf(" "); }
-    printf("  ___ __ ____ ____  __     ____ ____ ____  ___  __  _  _ ____ ____ __ _  __  ____  __ \n");
-    for(int  i = 0; i < 15; i++) { printf(" "); }
-    printf(" \/ __)  (  _ (  _ \\/ _\\   (    (  __) ___)\/ __)\/  \\\/ )( (  __|  _ (  ( \\\/ _\\(    \\\/ _\\ \n");
-    for(int  i = 0; i < 15; i++) { printf(" "); }
-    printf("( (_(  O ) _ ()   \/    \\   ) D () _)\\___ ( (_ (  O ) \\\/ \/) _) )   \/    \/    \\) D (    \\ \n");
-    for(int  i = 0; i < 15; i++) { printf(" "); }
-    printf(" \\___)__(____(__\\_)_\/\\_\/  (____(____|____\/\\___\/\\__\/ \\__\/(____|__\\_)_)__)_\/\\_(____|_\/\\_\/ \n");
+    for(int i = 0; i < tamArr; i++) {
+        for(int  j = 0; j < 10; j++) { printf(" "); }
+        printf("%s\n",titulo[i]);
+    }
     for(int i = 0; i < 2; i++) { printf("\n"); }
     for(int  i = 0; i < 45; i++) { printf(" "); }
-
     printf("Pressione <ENTER> para jogar");
     getc(stdin);
 
@@ -37,12 +41,45 @@ void telaTitulo() {
 
 //Tela de instruções para o jogador
 void telaInstrucao() {
-    printf("ai meu cu\n");
+    char telaComando();
+    char *textoInstr[] = {
+        "O jogador deve:",
+        "1. Indicar quantas e quais casas deverão receber os obstáculos.",
+        "2. Indicar a posição da cabeça da minhoca no tabuleiro."
+    };
+    int tamArr = sizeof(textoInstr)/sizeof(textoInstr[0]);
+
+    for(int i = 0; i < 10; i++) { printf("\n"); }
+    for(int i = 0; i < 50; i++) { printf(" "); }
+    for(int i = 0; i < 16; i++) { printf("="); }
+    printf("\n");
+    for(int i = 0; i < 50; i++) { printf(" "); }
+    printf("|| INSTRUÇÕES ||\n");
+    for(int i = 0; i < 50; i++) { printf(" "); }
+    for(int i = 0; i < 16; i++) { printf("="); }
+    for(int i = 0; i < 3; i++)  { printf("\n"); }
+    for(int i = 0; i < tamArr; i++) {
+        for(int j = 0; j < 40; j++) { printf(" "); }
+        printf("%s\n",textoInstr[i]);
+    }
+    for(int i = 0; i < 3; i++) { printf("\n"); }
+    for(int  i = 0; i < 45; i++) { printf(" "); }
+    printf("Pressione <ENTER> para continuar");
+    getc(stdin);
+    //Limpa a tela
+    system("cls");
+
+    telaComando();
 }
 
-/*
-void telaComando() {}
-*/
+//Pede ao jogador as coordenadas dos obstáculos
+char telaComando() {
+
+    printf("ai meu cu\n");
+
+    //return ;
+}
+
 
 /*
 
@@ -74,6 +111,8 @@ int main() {
 
     const char minhoca[] = "C***•";
     char tab[tamTab][tamTab];
+
+    setlocale(LC_ALL,"");
 
     for(int i = 0; i != tamTab; i++) {
         for(int j = 0; j != tamTab; j++) { tab[i][j] = ' '; }
